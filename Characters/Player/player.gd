280233084ac3059d67a4a84601dce45ec6639233
Peter Tasker <petertasker05@gmail.com>
2025-05-06 15:31:28 +0100

    Initial commit

---
+++ Characters/Player/player.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D  
+
+var speed = 100
+var animation_player
+
+func _ready():
+	# Get reference to the AnimationPlayer node
+	animation_player = $AnimatedSprite2D 
+
+func _physics_process(delta):
+	# Get input direction
+	var direction = Vector2.ZERO
+	
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+		animation_player.play("right")
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+		animation_player.play("left")
+	if Input.is_action_pressed("ui_down"):
+		direction.y += 1
+		animation_player.play("down")
+	if Input.is_action_pressed("ui_up"):
+		direction.y -= 1
+		animation_player.play("up")
+	
+	# Handle diagonal movement (prioritize the last pressed direction for animation)
+	if direction.length() > 0:
+		direction = direction.normalized()
+	else:
+		# Play idle animation or stop current animation when not moving
+		animation_player.stop()
+	
+	# Set velocity and move
+	velocity = direction * speed
+	move_and_slide()
